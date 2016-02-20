78650a5acd1ffd27e6f9ce3fb6e6665b34bcf1cb
uicab2593 <uicab2593@gmail.com>
2016-02-20 15:12:07 -0500

    telnet

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
 var telnet = require('./telnet');
+var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-  res.send('Hola Alis!');
+  res.sendFile(__dirname+'/views/index.html');
 });
 
-app.listen(3000, function () {
+// io.on('connection', function(socket){
+// });
+
+telnet.runTcpServer(io);
+
+http.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
--- telnet.js
+++ telnet.js
@@ -1,13 +1,14 @@
 var net = require('net');
 
-function onConect(socket) {
-    socket.on('data', function (data) {
-        socket.write('echo: ' + data);
-    });
-}
-
-module.export = {
-	runTcpServer: function(){
+module.exports = {
+	runTcpServer: function(io){
+		var onConect = 
+		function onConect(socket) {
+			socket.on('data', function (data) {
+				io.emit('signal',data.toString());
+				// socket.write('echo: ' + data);
+			});
+		}
 		net.createServer(onConect).listen(1337,'0.0.0.0')
 	}
 }
