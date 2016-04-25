611a962974ca219eb23453723b2766207834c253
CristhianPG <cristhianperez695@gmail.com>
2016-04-25 11:01:28 -0500

    origin/master

--- controllers/dictado.js
+++ controllers/dictado.js
@@ -1,3 +1,13 @@
-exports.index = function(req, res){
-  res.render('dictado');
+var bot = require('telegram-bot-bootstrap');
+var fs = require('fs');
+var botALIS = new bot('210304290:AAFymxNSeZs6ilUPCsYrPCDJxhQmeQhdo18');
+
+exports.index = function(req, res){  	
+	res.render('dictado');
 };
+
+exports.sendMessageToContact = function (req, res) {
+	var idChat = req.param("idChat");
+	var msg = req.param("msgToSend");
+	botALIS.sendMessage({chat_id: idChat, text: msg});
+}
